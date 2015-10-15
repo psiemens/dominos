62d30f6ba639d7d5554f4ece238d02f0512aca58
Peter Siemens <peterjsiemens@gmail.com>
2015-10-14 22:04:40 -0700

    Add all options, configure place-order endpoint

--- dominos.py
+++ dominos.py
@@ -3,10 +3,10 @@ import requests
 import json
 
 @click.command()
-@click.option('--address', prompt=True)
-@click.option('--city', prompt=True)
-@click.option('--province', prompt=True)
-@click.option('--postal_code', prompt=True)
+@click.option("--address", prompt=True)
+@click.option("--city", prompt=True)
+@click.option("--province", prompt=True)
+@click.option("--postal_code", prompt=True)
 
 class Dominos(object):
 
@@ -15,12 +15,12 @@ class Dominos(object):
 		self.order = DominosOrder()
 
 		try:	
-			self.place_order(address, city, province, postal_code)
+			self.make_order(address, city, province, postal_code)
 		except PizzaProblem as e:
 			click.echo(str(e))
 			return
 
-	def place_order(self, address, city, province, postal_code):
+	def make_order(self, address, city, province, postal_code):
 
 		self.select_store(address, city, province, postal_code)
 
@@ -32,25 +32,23 @@ class Dominos(object):
 
 		self.price_order()
 
+		self.enter_information()
+
 		if click.confirm("Your total is $%s. Place order?" % self.order.get_total(), abort=True):
-			click.echo('Your order has been placed!')
-
-		self.provide_information()
-
-		click.echo(self.order.json())
-
+			self.place_order()
+			click.echo("Your order has been placed!")
 
 	def select_store(self, address, city, province, postal_code):
 
 		payload = {
-			'type': 'Delivery',
-			'c': "%s, %s %s" % (city, province, postal_code), 
-			's': address
+			"type": "Delivery",
+			"c": "%s, %s %s" % (city, province, postal_code), 
+			"s": address
 		}
 
-		response = requests.get('https://order.dominos.ca/power/store-locator?type=Delivery', params=payload).json()
+		response = requests.get("https://order.dominos.ca/power/store-locator?type=Delivery", params=payload).json()
 
-		self.order.set_address(response['Address'])
+		self.order.set_address(response["Address"])
 
 		stores = response["Stores"]
 
@@ -58,12 +56,12 @@ class Dominos(object):
 			raise PizzaProblem("Sorry, there are no Domino's locations near you.")
 
 		for n, store in enumerate(stores):
-			click.echo("%d) %s" % (n+1, store["AddressDescription"].split('\n')[0]))
+			click.echo("%d) %s" % (n+1, store["AddressDescription"].split("\n")[0]))
 
-		store_index = click.prompt('Choose a location', type=int)
+		store_index = click.prompt("Choose a location", type=int)
 
 		if store_index < 1 or store_index > len(stores):
-			raise PizzaProblem('Not a valid store option.')
+			raise PizzaProblem("Not a valid store option.")
 
 		self.order.set_store(stores[store_index-1])
 
@@ -71,17 +69,17 @@ class Dominos(object):
 
 		def choose_product(pizzas=[]):
 			pizza = {}
-			pizza['size'] = click.prompt('Choose a size (s, m, l)', type=str)
+			pizza["size"] = click.prompt("Choose a size (s, m, l)", type=str)
 
 			def choose_toppings():
-				choices = click.prompt('Choose toppings (comma-separated list). Type "options" for selection', type=str)
-				if choices == 'options':
-					click.echo("cheese, pepperoni, ham, pineapple")
+				choices = click.prompt("Choose toppings (comma-separated list). Type ls for options", type=str)
+				if choices == "ls":
+					click.echo(", ".join(DominosOrder.OPTIONS.keys()))
 					return choose_toppings()
 				else:
-					return choices.split(',')
+					return choices.split(",")
 
-			pizza['toppings'] = choose_toppings()
+			pizza["toppings"] = choose_toppings()
 
 			pizzas.append(pizza)
 
@@ -93,21 +91,58 @@ class Dominos(object):
 		self.order.set_products(choose_product())
 
 	def validate_order(self):
-		response = requests.post("https://order.dominos.ca/power/validate-order", json=self.order.json())
+		response = requests.post("https://order.dominos.ca/power/validate-order", json=self.order.json(), timeout=(10.0, 10.0))
 
 	def price_order(self):
 		response = requests.post("https://order.dominos.ca/power/price-order", json=self.order.json())
 		self.order.set_price(response.json())
 
-	def provide_information(self):
-		first_name = click.prompt('First name', type=str)
-		last_name = click.prompt('Last name', type=str)
-		phone = click.prompt('Phone number', type=str)
-		email = click.prompt('Email', type=str)
+	def place_order(self):
+		response = requests.post("https://order.dominos.ca/power/place-order", json=self.order.json())
 
+	def enter_information(self):
+		first_name = click.prompt("First name", type=str)
+		last_name = click.prompt("Last name", type=str)
+		phone = click.prompt("Phone number", type=str)
+		email = click.prompt("Email", type=str)
 		self.order.set_information(first_name, last_name, phone, email)
 
 class DominosOrder(object):
+
+	SIZES = {
+		"s": "10SCREEN",
+		"m": "12SCREEN",
+		"l": "14SCREEN"
+	}
+
+	OPTIONS = {
+		# Meats
+		"cheese": 				"C",
+		"pepperoni": 			"P",
+		"brooklyn pepperoni": 	"Xp",
+		"sausage":				"S",
+		"beef": 				"B",
+		"ham": 					"H",
+		"bacon":				"K",
+		"salami":				"L",
+		"chicken":				"D",
+		"philly steak":			"St",
+		"anchovy":				"A",
+
+		# Non-meats
+		"cheddar/mozza":		"Cm",
+		"feta":					"Fe",
+		"provolone":			"Cp",
+		"banana peppers":		"Z",
+		"black olives":			"R",
+		"green olives":			"V",
+		"green peppers":		"G",
+		"mushroom":				"M",
+		"pineapple":			"N",
+		"onion":				"O",
+		"tomatoes":				"T",
+		"jalapeno peppers":		"J"
+	}
 
 	def __init__(self):
 
@@ -144,35 +179,25 @@ class DominosOrder(object):
 
 	def set_products(self, pizzas):
 
-		SIZES = {
-			's': '10SCREEN',
-			'm': '12SCREEN',
-			'l': '14SCREEN'
-		}
-
-		OPTIONS = {
-			'cheese': 'C',
-			'pepperoni': 'P',
-			'bacon': 'B',
-			'ham': 'H'
-		}
-
 		products = []
 
 		def build_options(toppings):
 			options = {}
+			print self.OPTIONS
 			for option in toppings:
-				options[OPTIONS[option]] = {'1/1': '1'}
+				option = option.strip()
+				if option in DominosOrder.OPTIONS:
+					options[DominosOrder.OPTIONS[option]] = {"1/1": "1"}
 			return options
 
 		for n, pizza in enumerate(pizzas):
 			products.append({
-				'Code': SIZES[pizza['size']],
-				'Qty': 1,
-				'ID': n,
-				'Instructions': '',
-				'isNew': True,
-				'Options': build_options(pizza['toppings']),
+				"Code": DominosOrder.SIZES[pizza["size"]],
+				"Qty": 1,
+				"ID": n,
+				"Instructions": "",
+				"isNew": True,
+				"Options": build_options(pizza["toppings"]),
 			})
 
 		self.order["Products"] = products
@@ -181,8 +206,10 @@ class DominosOrder(object):
 		self.order["Amounts"] = response["Order"]["Amounts"]
 
 	def set_information(self, first_name, last_name, phone, email):
-		#self.order["First"]
-		pass
+		self.order["FirstName"] = first_name
+		self.order["LastName"] = last_name
+		self.order["Email"]	= email
+		self.order["Phone"]	= phone
 
 	def get_total(self):
 		return self.order["Amounts"]["Payment"]
@@ -193,5 +220,5 @@ class DominosOrder(object):
 class PizzaProblem(Exception):
     pass
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     d = Dominos()
